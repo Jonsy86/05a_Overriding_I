e1430029f4fb9978229ca38b3f3ccd3347083464
Jonsy86 <jonasnolte86@gmail.com>
2024-05-27 12:06:51 +0200

    überschreiben

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,12 @@ public class App {
     
     public static void main(String[] args) {
      
-       
+     
+        
+        Child child = new Child();
+            output(child.sayHello());
+
+
        
 
     }
---
+++ com/cc/java/Parent.java
@@ -0,0 +1,16 @@
+package com.cc.java;
+
+public class Parent {
+
+
+    public String sayHello(){
+        return "Hello from Parent!";
+        
+        
+    }
+
+
+
+
+
+}
